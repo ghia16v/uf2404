723c877e99fb770c3b92e9b6152dd310d610d237
Koldo Iglesias <info@koldoiglesias.com>
2020-03-10 13:25:58 +0100

    hhooolaaa

--- src/jotaiunit/Principal.java
+++ src/jotaiunit/Principal.java
@@ -6,13 +6,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		operacion(3, 5);
+		// operacion(3, 5);
 
 	}
 
 	public static double operacion(int numero, int numero2) {
 		String operacion;
-		double resultado;
+		double resultado = 0;
 		Scanner scanop = new Scanner(System.in);
 
 		System.out.println("Método que suma dos valores recibiéndolos por parámetro");
@@ -25,26 +25,49 @@ public class Principal {
 			System.out.println("Has elegido sumar");
 			resultado = numero + numero2;
 			System.out.println("La suma es " + (resultado));
-			return numero;
+			return resultado;
 
 		case "R":
 			System.out.println("Has elegido restar");
 			resultado = numero - numero2;
 			System.out.println("La suma es " + (resultado));
-			return numero;
+			return resultado;
 		case "M":
 			System.out.println("Has elegido multiplicar");
 			resultado = numero * numero2;
 			System.out.println("La suma es " + (resultado));
-			return numero;
+			return resultado;
 		case "D":
 			System.out.println("Has elegido dividir");
 			resultado = numero / numero2;
 			System.out.println("La suma es " + (resultado));
-			return numero;
+			return resultado;
 		default:
 			System.out.println("No has escogido una operación válida");
+			return resultado;
+		}
 
+	}
+
+	public static String metodoestring() {
+
+		String cadena1 = "Hola ";
+		String cadena2 = "amiguitos ";
+		String cadena3 = "del progressive";
+		return cadena1 + cadena2 + cadena3;
+	}
+
+	public static String esBisiesto(int año) {
+		String resultado;
+
+		if (año % 4 == 0) {
+			resultado = "SÍ es bisiesto";
+			System.out.println(resultado);
+			return resultado;
+		} else {
+			resultado = "NO es bisiesto";
+			System.out.println(resultado);
+			return resultado;
 		}
 
 	}
--- src/jotaiunit/TestPrincipal.java
+++ src/jotaiunit/TestPrincipal.java
@@ -1,14 +1,35 @@
 package jotaiunit;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class TestPrincipal {
 
+//	@Test
+//	public void testOperacion() { // Testea la multiplicación
+//		double resultado = Principal.operacion(10, 5);
+//		double esperado = 50d;
+//
+//		assertEquals(esperado, resultado, 0); // delta es la diferencia máxima entre resultado y esperado
+
+//	}
+
 	@Test
-	public void testOperacion() {
-		assert fail("Not yet implemented");
+	public void testMetodoestring() {
+		String resultado = Principal.metodoestring();
+		String esperado = "Hola amiguitos del progressive";
+
+		assertEquals("Hola amiguitos del progressive", esperado, resultado);
+	}
+
+	@Test
+	public void testEsBisiesto() {
+		String resultado = Principal.esBisiesto(2004);
+		String esperado = "SÍ es bisiesto";
+
+		assertEquals("Es bisiesto", esperado, resultado);
+
 	}
 
 }
